6927a84559503d18f4f4e0585ba71fb9a9feea7d
Salaxy <salaxy@web.de>
2013-11-25 00:19:13 +0100

    sql-dump correction

--- PaperFlyServer-account/src/main/resources/META-INF/insert.sql
+++ PaperFlyServer-account/src/main/resources/META-INF/insert.sql
@@ -5,11 +5,11 @@
 
 LOCK TABLES `ACCOUNT` WRITE;
 /*!40000 ALTER TABLE `ACCOUNT` DISABLE KEYS */;
-INSERT INTO `ACCOUNT` VALUES ('mail@mail.de','2013-10-30 11:09:26',1,'firstName','2013-10-30 11:09:26','lastName','username');
-INSERT INTO `ACCOUNT` VALUES ('friend2db@mail.de','2013-10-30 11:09:26',1,'firstName2','2013-10-30 11:09:26','lastName2','username2');
-INSERT INTO `ACCOUNT` VALUES ('friend3db@mail.de','2013-10-30 11:09:26',1,'firstName3','2013-10-30 11:09:26','lastName3','username3');
-INSERT INTO `ACCOUNT` VALUES ('friend4db@mail.de','2013-10-30 11:09:26',1,'firstName4','2013-10-30 11:09:26','lastName4','username4');
-INSERT INTO `ACCOUNT` VALUES ('friend5db@mail.de','2013-10-30 11:09:26',1,'firstName5','2013-10-30 11:09:26','lastName5','username5');
+INSERT INTO `ACCOUNT` VALUES ('mail@mail.de','2013-10-30 11:09:26',1,'firstName','2013-10-30 11:09:26','lastName',1,'username');
+INSERT INTO `ACCOUNT` VALUES ('friend2db@mail.de','2013-10-30 11:09:26',1,'firstName2','2013-10-30 11:09:26','lastName2',1,'username2');
+INSERT INTO `ACCOUNT` VALUES ('friend3db@mail.de','2013-10-30 11:09:26',1,'firstName3','2013-10-30 11:09:26','lastName3',1,'username3');
+INSERT INTO `ACCOUNT` VALUES ('friend4db@mail.de','2013-10-30 11:09:26',1,'firstName4','2013-10-30 11:09:26','lastName4',1,'username4');
+INSERT INTO `ACCOUNT` VALUES ('friend5db@mail.de','2013-10-30 11:09:26',1,'firstName5','2013-10-30 11:09:26','lastName5',1,'username5');
 /*!40000 ALTER TABLE `ACCOUNT` ENABLE KEYS */;
 UNLOCK TABLES;
 
